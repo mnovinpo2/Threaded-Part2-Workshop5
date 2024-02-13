704bca90801d86216b0f2ac4efa363ba2efcde08
mohsen novin pour <mnovinpo2@gmail.com>
2024-02-13 12:02:46 -0700

    updated contact page orderby firstname then lastname

--- TravelExpertsMVC/Controllers/AccountController.cs
+++ TravelExpertsMVC/Controllers/AccountController.cs
@@ -10,11 +10,12 @@ namespace TravelExpertsMVC.Controllers
     public class AccountController : Controller
     {
         private TravelExpertsContext? db { get; set; }
-        
+
         public AccountController(TravelExpertsContext db)
         {
             this.db = db;
         }
+
         public IActionResult Login(string returnUrl = "")
         {
             if (!string.IsNullOrEmpty(returnUrl))
@@ -25,53 +26,58 @@ namespace TravelExpertsMVC.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> LoginAsync(Customer cust) // data collected from the form
+        public async Task<IActionResult> LoginAsync(Customer cust)
         {
-            Customer cst = CustomerDB.Authenticate(db, cust.Username, cust.Password);
-            if (cst == null) // no matching username/password
+            try
             {
-                return View();
-            }
-            //TempData["CustomerId"] = cst.ID;
-            HttpContext.Session.SetInt32("CustomerId", cst.CustomerId);
+                Customer cst = CustomerDB.Authenticate(db, cust.Username, cust.Password);
 
-            List<Claim> claims = new List<Claim>
-            {
-                new Claim(ClaimTypes.Name, cst.Username),
-                new Claim("FirstName", cst.CustFirstName),
-                new Claim("LastName", cst.CustLastName),
-                new Claim("CustomerId", cst.CustomerId.ToString())
-            };
-            ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims,
-                CookieAuthenticationDefaults.AuthenticationScheme); // cookies authentication
-            ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+                if (cst == null)
+                {
+                    ModelState.AddModelError("", "Invalid username or password");
+                    return View();
+                }
 
-            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
-                claimsPrincipal);
 
-            string? returnUrl = TempData["ReturnUrl"]?.ToString();
-            if (string.IsNullOrEmpty(returnUrl))
-            {
+                HttpContext.Session.SetInt32("CustomerId", cst.CustomerId);
+
+                List<Claim> claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, cst.Username),
+                    new Claim("FirstName", cst.CustFirstName),
+                    new Claim("LastName", cst.CustLastName),
+                    new Claim("CustomerId", cst.CustomerId.ToString())
+                };
+
+                ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsPrincipal);
+
                 return RedirectToAction("Index", "Home");
             }
-            else
+            catch (Exception)
             {
-                return Redirect(returnUrl);
+                ModelState.AddModelError("", "An error occurred during authentication.");
+                return View();
             }
         }
+
         [HttpPost]
         public async Task<IActionResult> LogoutAsync()
         {
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
             HttpContext.Session.Remove("CustomerId");
             HttpContext.Session.Clear();
-            return RedirectToAction( "Index", "Home");
+            return RedirectToAction("Index", "Home");
         }
+
         [HttpGet]
         public ActionResult Register()
         {
             return View();
         }
+
         [HttpPost]
         public ActionResult Register(Customer customer)
         {
@@ -84,9 +90,7 @@ namespace TravelExpertsMVC.Controllers
             catch (Exception)
             {
                 return RedirectToAction("Index", "Home");
-
             }
-
         }
     }
 }
