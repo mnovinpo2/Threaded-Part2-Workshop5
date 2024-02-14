4dfa09d9a1303423e7781ac1c6ae2d37a0bea487
Moose <mustafa.hersi@edu.sait.ca>
2024-02-14 11:18:51 -0700

    started on a new page

--- TravelExpertsMVC/Controllers/HomeController.cs
+++ TravelExpertsMVC/Controllers/HomeController.cs
@@ -26,6 +26,10 @@ namespace TravelExpertsMVC.Controllers
 		{
 			return View();
 		}
+		public IActionResult Offers()
+		{
+			return View();
+		}
 
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
